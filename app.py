0039e0e12def19387cc4ca96046263ec4e8c53cd
Amr Mohammed <abdelrahman.mohamed89@msa.edu.eg>
2025-03-10 08:09:56 +0200

    first time

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import pickle
+
+tokenizer = pickle.load(open('models/cv.pkl', 'rb'))
+model = pickle.load(open('models/clf.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+   text = request.form.get('email-content')
+   tokens = tokenizer.transform([text])
+   prediction = model.predict(tokens)
+   prediction = 1 if prediction == 1 else -1
+   return render_template('index.html', prediction=prediction, text=text)
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0')
