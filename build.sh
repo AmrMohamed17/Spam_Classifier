cf5b1a8a2eda076ea00978a045830dc233ef9d7c
Amr Mohammed <abdelrahman.mohamed89@msa.edu.eg>
2025-03-14 06:25:11 +0200

    Optimized Vercel deployment and reduced dependencies

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-pip install --no-cache-dir --no-deps -r requirements.txt
+#!/bin/bash
+# Install only necessary packages with minimal dependencies
+pip install --no-cache-dir -r requirements.txt --no-deps
+pip install --no-cache-dir numpy==1.21.4
+pip install --no-cache-dir scipy==1.7.3 --no-deps
